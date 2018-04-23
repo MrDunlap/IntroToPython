fc386d3da995701322a130f6f2751fb85f6fcfa9
Stephen Dunlap <Stephen@SD.local>
2018-04-22 21:26:13 -0600

    fixed domain

--- Practice_Code.py
+++ Practice_Code.py
@@ -6,5 +6,5 @@ for countdown in range (5,-1,-1):
 
 #Emulating Numeric Types
 import matplotlib.pyplot as plt
-plt.plot(x=[0,1,2,3,4], y = [0.5,1,1.5,2,2.5])
+plt.plot([0,1,2,3,4],[0,1,2,3,4])
 plt.show()
